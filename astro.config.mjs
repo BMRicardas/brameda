56e6e19d573f2a9deb28b896d7e34b32b12eb4f4
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-10-31 21:59:41 +0200

    add environment variable schema for Contentful and Web3Forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,29 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
+  env: {
+    schema: {
+      CONTENTFUL_SPACE_ID: envField.string({
+        access: "secret",
+        context: "server",
+      }),
+      CONTENTFUL_DELIVERY_TOKEN: envField.string({
+        access: "secret",
+        context: "server",
+      }),
+      CONTENTFUL_PREVIEW_TOKEN: envField.string({
+        access: "secret",
+        context: "server",
+      }),
+      WEB3FORMS_PUBLIC_ACCESS_KEY: envField.string({
+        access: "secret",
+        context: "server",
+      }),
+    },
+  },
   image: {
     domains: ["images.ctfassets.net"],
     remotePatterns: [
