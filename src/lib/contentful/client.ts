574864fc10ca675c408a8e4b26cc8349c77ea643
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-11-01 09:36:11 +0200

    remove unused Contentful client code: delete unnecessary import and type definition

--- src/lib/contentful/client.ts
+++ src/lib/contentful/client.ts
@@ -1,8 +1,5 @@
 import { createClient } from "contentful";
-
 import { createClientParams } from "@/lib/contentful/constants";
 
 export const contentfulClient =
   createClient(createClientParams).withoutUnresolvableLinks;
-
-export type ContentfulClient = typeof contentfulClient;
