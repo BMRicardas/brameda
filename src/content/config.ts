3d3931db5da5bf0af78570b84297487688077c6c
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-11-01 11:41:43 +0200

    refactor(content): update contentfulLoader to use environment variables for configuration

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,20 @@
 import { defineCollection } from "astro:content";
 import { productSchema } from "@/schemas/contentful";
-import { createClientParams } from "@/lib/contentful/constants";
 import { contentfulLoader } from "@/lib/contentful/loader";
+import {
+  CONTENTFUL_DELIVERY_TOKEN,
+  CONTENTFUL_PREVIEW_TOKEN,
+  CONTENTFUL_SPACE_ID,
+} from "astro:env/server";
 
 const products = defineCollection({
-  loader: contentfulLoader(createClientParams),
+  loader: contentfulLoader({
+    space: CONTENTFUL_SPACE_ID,
+    accessToken: import.meta.env.DEV
+      ? CONTENTFUL_PREVIEW_TOKEN
+      : CONTENTFUL_DELIVERY_TOKEN,
+    host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+  }),
   schema: productSchema,
 });
 
