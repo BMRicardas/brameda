5099367693ada7f2cfceb7f435ca687ff3859c0e
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-11-01 14:18:45 +0200

    refactor(content): simplify contentfulLoader configuration by using contentfulConfig

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,11 @@
 import { defineCollection } from "astro:content";
 import { productSchema } from "@/schemas/contentful";
 import { contentfulLoader } from "@/lib/contentful/loader";
-import {
-  CONTENTFUL_DELIVERY_TOKEN,
-  CONTENTFUL_PREVIEW_TOKEN,
-  CONTENTFUL_SPACE_ID,
-} from "astro:env/server";
+import { contentfulConfig } from "@/lib/contentful/constants";
 
 const products = defineCollection({
-  loader: contentfulLoader({
-    space: CONTENTFUL_SPACE_ID,
-    accessToken: import.meta.env.DEV
-      ? CONTENTFUL_PREVIEW_TOKEN
-      : CONTENTFUL_DELIVERY_TOKEN,
-    host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
-  }),
+  loader: contentfulLoader(contentfulConfig),
+  // type: "data",
   schema: productSchema,
 });
 
