6369e4005d9a3bf4cc3540f7a9d43141dd679748
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-11-03 21:27:01 +0200

    feat(eslint): add perfectionist plugin and update import sorting rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,17 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import perfectionist from "eslint-plugin-perfectionist";
+
 export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginAstro.configs.all,
   ...eslintPluginAstro.configs["flat/jsx-a11y-strict"],
   {
+    plugins: {
+      perfectionist,
+    },
     rules: {
-      "sort-imports": [
-        "error",
-        {
-          allowSeparatedGroups: true,
-        },
-      ],
+      "perfectionist/sort-imports": "error",
     },
   },
 ];
