c97da577357d0d2e02642817d3713c34c3b7f6ee
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-11-03 21:48:56 +0200

    feat(astro.config): change inlineStylesheets option to always for build configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig, envField } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   build: {
-    inlineStylesheets: "never",
+    inlineStylesheets: "always",
   },
   output: "static",
   env: {
